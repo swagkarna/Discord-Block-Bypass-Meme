05388fef4a91468f4bdac6696ebf731d022dc18c
Yaekith <47884923+Yaekith@users.noreply.github.com>
2020-07-12 10:49:32 +0800

    Files

---
+++ MessageBody.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBlockBypass
+{
+    public class MessageBody
+    {
+        public string content { get; set; }
+
+        public string nonce { get; set; }
+
+        public bool tts { get; set; }
+    }
+}
